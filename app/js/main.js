e0d613f2350225344d5014d0206556f6bc4d3189
SpensDee <spensdee@gmail.com>
2021-04-20 22:49:53 +0300

    lesson
    1

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+
+//вывод текста
+new Vue({
+    el: '#a',
+    data: {
+        vue: 'first message'
+    }
+});
+
+//вывод на экран данных из инпут
+new Vue ({
+    el: '#b',
+    data: {
+        name: 'Hello World'
+    }
+});
+
